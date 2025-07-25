d92d7f844bac59e580b6fa8f68704eab2bc08f25
Cam Cook <cam.cook.codes@gmail.com>
2025-07-25 14:09:52 -0400

    ci: introduce

--- test/support/test_behaviours.ex
+++ test/support/test_behaviours.ex
@@ -1,14 +1,17 @@
 defmodule TestBehaviorA do
+  @moduledoc false
   @callback test_a() :: :ok
   @callback function_a(term()) :: term()
 end
 
 defmodule TestBehaviorB do
+  @moduledoc false
   @callback test_b() :: :error
   @callback function_b(term()) :: term()
 end
 
 defmodule OptionalCallbackBehavior do
+  @moduledoc false
   @callback required_callback() :: :ok
   @callback optional_callback() :: :optional
   @optional_callbacks [optional_callback: 0]
