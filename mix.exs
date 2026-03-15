8c40f7aee913cc7d6ee0aa7f123a9871d5f34657
Cam Cook <cam.cook.codes@gmail.com>
2026-03-15 09:13:04 -0400

    build: v1.0.1

--- mix.exs
+++ mix.exs
@@ -2,24 +2,17 @@ defmodule WellBehaved.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/camatcode/well_behaved"
-  @version "1.0.0"
+  @version "1.0.1"
 
   def project do
     [
       app: :well_behaved,
       version: @version,
-      elixir: "~> 1.18",
+      elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test,
-        "coveralls.cobertura": :test
-      ],
       # Hex
       package: package(),
       description: """
@@ -72,12 +65,24 @@ defmodule WellBehaved.MixProject do
     ]
   end
 
+  def cli do
+    [
+      preferred_envs: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
+    ]
+  end
+
   defp deps do
     [
       {:excoveralls, "~> 0.18", only: [:test]},
-      {:quokka, "~> 2.8", only: [:dev, :test], runtime: false},
+      {:quokka, "~> 2.12", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.37", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false}
     ]
   end
 
